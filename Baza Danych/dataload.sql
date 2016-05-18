c36b8c7d1b9f4bb8fb0d961b45ca16cdfcd6d742
shortname <dymonjakub@gmail.com>
2016-05-18 15:41:31 +0200

    Dane testowe dla magazynów i personelu

--- Baza Danych/dataload.sql
+++ Baza Danych/dataload.sql
@@ -2,13 +2,15 @@
 INSERT INTO `kontaLogowania` (`id`, `login`, `sha256Haslo`, `ostatnieLogowanie`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', NULL),
 (2, 'client', '62608e08adc29a8d6dbc9754e659f125', NULL),
-(3, 'personnel', 'f82366a9ddc064585d54e3f78bde3221', NULL);
+(3, 'personnel', 'f82366a9ddc064585d54e3f78bde3221', NULL),
+(4, 'personnel2', 'f82366a9ddc064585d54e3f78bde3221', NULL);
 
 #adresy
 INSERT INTO `adresy`(`id`, `miasto`, `wojewodztwo`, `kodPocztowy`, `ulica`, `nrDomu`, `nrLokalu`) VALUES
 (1, 'Wałbrzych', 'dolnośląskie', '90-901', 'al. Szkolna', '2', NULL),
 (2, 'Kielce', 'świętokrzyskie', '09-999', 'pl. Jednokierunkowa', '11', '4'),
-(3, 'Zielona Góra', 'lubuskie', '19-999', 'ul. Magazynowa', '6a', '2');
+(3, 'Zielona Góra', 'lubuskie', '19-999', 'ul. Magazynowa', '6a', '2'),
+(4, 'Zielona Góra', 'lubuskie', '19-000', 'ul. Magazynowa', '182', '2');
 
 #kontakty
 INSERT INTO `kontakty`(`id`, `telefon1`, `telefon2`, `email`) VALUES
@@ -19,3 +21,11 @@ INSERT INTO `kontakty`(`id`, `telefon1`, `telefon2`, `email`) VALUES
 #klienci
 INSERT INTO `klienci`(`id`, `Imie`, `Nazwisko`, `idKontoLogowania`, `idAdres`, `idKontakt`, `nip`) VALUES
 (1, 'Jan', 'Wąski', 2, 1, 1, '789789798789789');
+
+#magazyny
+INSERT INTO `magazyny`(`id`, `idAdres`) VALUES (1,3);
+
+#personel
+INSERT INTO `personel`(`id`, `Imie`, `Nazwisko`, `idKontoLogowania`, `idAdres`, `idKontakt`, `idMagazyn`, `kierownik`) VALUES
+(1, 'Zygfryd', "Mariacki", 3, 2, 2, 1, 0),
+(2, 'Aleksandra', "Drwal", 4, 3, 3, 1, 1);
