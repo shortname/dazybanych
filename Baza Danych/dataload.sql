ad5ae4e0455303ad8fb4d6304e6847702cbc3707
shortname <dymonjakub@gmail.com>
2016-05-18 16:58:26 +0200

    Wszystkie dane testowe poza sprzedaz i platnosci (celowo), stare pliki php przeniesione.

--- Baza Danych/dataload.sql
+++ Baza Danych/dataload.sql
@@ -1,9 +1,9 @@
 #konta logowania
-INSERT INTO `kontaLogowania` (`id`, `login`, `sha256Haslo`, `ostatnieLogowanie`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', NULL),
-(2, 'client', '62608e08adc29a8d6dbc9754e659f125', NULL),
-(3, 'personnel', 'f82366a9ddc064585d54e3f78bde3221', NULL),
-(4, 'personnel2', 'f82366a9ddc064585d54e3f78bde3221', NULL);
+INSERT INTO `kontaLogowania` (`id`, `login`, `sha256Haslo`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3'),
+(2, 'client', '62608e08adc29a8d6dbc9754e659f125'),
+(3, 'personnel', 'f82366a9ddc064585d54e3f78bde3221'),
+(4, 'personnel2', 'f82366a9ddc064585d54e3f78bde3221');
 
 #adresy
 INSERT INTO `adresy`(`id`, `miasto`, `wojewodztwo`, `kodPocztowy`, `ulica`, `nrDomu`, `nrLokalu`) VALUES
@@ -29,3 +29,31 @@ INSERT INTO `magazyny`(`id`, `idAdres`) VALUES (1,3);
 INSERT INTO `personel`(`id`, `Imie`, `Nazwisko`, `idKontoLogowania`, `idAdres`, `idKontakt`, `idMagazyn`, `kierownik`) VALUES
 (1, 'Zygfryd', "Mariacki", 3, 2, 2, 1, 0),
 (2, 'Aleksandra', "Drwal", 4, 3, 3, 1, 1);
+
+#producenci
+INSERT INTO `producenci`(`id`, `nazwaProducenta`) VALUES
+(1, 'WIERTEX'),
+(2, 'WINDPOL'),
+(3, 'ZIMBABWE CONTINENTAL');
+
+#kategorie
+INSERT INTO `kategorie`(`id`, `nazwaKategorii`) VALUES
+(1, 'football'),
+(2, 'tenis'),
+(3, 'pływanie');
+
+#produkty
+INSERT INTO `produkty`(`id`, `nazwa`, `idProducenta`, `idKategorii`, `opis`, `cenaNetto`, `cenaBrutto`) VALUES
+(1, 'piłka zielona', 1, 1, 'piłka, zielona, okrągła, do footbolu', 0.10, 0.90),
+(2, 'piłka czerwona', 1, 1, 'piłka, czerwona, okrągła, do footbolu', 0.80, 1.70),
+(3, 'piłka niebieska', 2, 1, 'piłka, niebieska, okrągła, do footbolu', 105.90, 180.4),
+(4, 'rakieta', 3, 2, 'czarna, z grafenu', 72.0, 102.86),
+(5, 'siatka', 3, 2, '100m, nylonowa, biała', 7.20, 12.86);
+
+#zaopatrzenie
+INSERT INTO `zaopatrzenie`(`id`, `idProduktu`, `idMagazynu`, `ilosc`) VALUES
+(1, 1, 1, 100000),
+(2, 1, 1, 10),
+(3, 2, 1, 10),
+(4, 3, 1, 15),
+(5, 4, 1, 1);
