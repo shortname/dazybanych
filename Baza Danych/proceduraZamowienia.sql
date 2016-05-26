195d130ee323e9dc474a91e5c827233db75fc3d4
Kuba Dymon <dymonjakub@gmail.com>
2016-05-26 12:28:19 +0200

    Dodawanie produktów do zamówienia działa, tworzenie zamówienia tymczasowe, nadal brak zabezpieczeń.

--- Baza Danych/proceduraZamowienia.sql
+++ Baza Danych/proceduraZamowienia.sql
@@ -2,21 +2,30 @@ DELIMITER //
 CREATE PROCEDURE dodajProduktDoZamowienia(
   IN id_produktu INT UNSIGNED,
   IN il INT UNSIGNED,
+  IN id_klienta INT UNSIGNED,
   OUT wynik INT UNSIGNED
 )
 BEGIN
   DECLARE zid INT UNSIGNED DEFAULT 0;
   DECLARE zil INT UNSIGNED DEFAULT 0;
   DECLARE zidm INT UNSIGNED DEFAULT 0;
+  DECLARE zidz INT UNSIGNED DEFAULT 0;
   DECLARE zap CURSOR FOR SELECT id, idMagazynu, ilosc FROM zaopatrzenie WHERE idProduktu = id_produktu AND ilosc > 0;
+  INSERT INTO zamowienia (idKlienta, status) VALUES (id_klienta, 'S');
+  SET zidz = LAST_INSERT_ID();
   OPEN zap;
-  FETCH zap INTO zid, zidm, zil;
-  IF zil <= il THEN
-    DELETE FROM zaopatrzenie WHERE id = zid;
-    SET il = il - zil;
-  ELSE
-    UPDATE zaopatrzenie SET ilosc = (zil - il) WHERE id = zid;
-    SET il = 0;
+  WHILE il > 0 DO
+    FETCH zap INTO zid, zidm, zil;
+    IF zil <= il THEN
+      DELETE FROM zaopatrzenie WHERE id = zid;
+      INSERT INTO zamowienia_produkty (idZamowienia, idMagazynu, idProduktu, ilosc) VALUES (zidz, zidm, id_produktu, zil);
+      SET il = il - zil;
+    ELSE
+      UPDATE zaopatrzenie SET ilosc = (zil - il) WHERE id = zid;
+      INSERT INTO zamowienia_produkty (idZamowienia, idMagazynu, idProduktu, ilosc) VALUES (zidz, zidm, id_produktu, il);
+      SET il = 0;
+    END IF;
+  END WHILE;
   CLOSE zap;
 END //
 DELIMITER ;
