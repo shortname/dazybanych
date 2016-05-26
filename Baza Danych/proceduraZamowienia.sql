f7d5151c2327968b58a7ce942335a687adbfaa8e
Kuba Dymon <dymonjakub@gmail.com>
2016-05-26 16:56:15 +0200

    Procedura dla całego zamówienia (in progress)

--- Baza Danych/proceduraZamowienia.sql
+++ Baza Danych/proceduraZamowienia.sql
@@ -1,18 +1,13 @@
-DELIMITER //
 CREATE PROCEDURE dodajProduktDoZamowienia(
   IN id_produktu INT UNSIGNED,
   IN il INT UNSIGNED,
   IN id_klienta INT UNSIGNED,
-  OUT wynik INT UNSIGNED
+  IN idZamowienia INT UNSIGNED
 )
 BEGIN
   DECLARE zid INT UNSIGNED DEFAULT 0;
   DECLARE zil INT UNSIGNED DEFAULT 0;
-  DECLARE zidm INT UNSIGNED DEFAULT 0;
-  DECLARE zidz INT UNSIGNED DEFAULT 0;
   DECLARE zap CURSOR FOR SELECT id, idMagazynu, ilosc FROM zaopatrzenie WHERE idProduktu = id_produktu AND ilosc > 0;
-  INSERT INTO zamowienia (idKlienta, status) VALUES (id_klienta, 'S');
-  SET zidz = LAST_INSERT_ID();
   OPEN zap;
   WHILE il > 0 DO
     FETCH zap INTO zid, zidm, zil;
@@ -28,4 +23,26 @@ BEGIN
   END WHILE;
   CLOSE zap;
 END //
-DELIMITER ;
+
+CREATE PROCEDURE tworzZamowienie(
+  IN id_produktow VARCHAR(255),
+  IN ilosci VARCHAR(255),
+  IN id_klienta INT UNSIGNED
+)
+BEGIN
+  DECLARE zidz INT UNSIGNED DEFAULT 0;
+  DECLARE il INT UNSIGNED DEFAULT 0;
+  DECLARE id_produktu INT UNSIGNED DEFAULT 0;
+  DECLARE zidm INT UNSIGNED DEFAULT 0;
+  INSERT INTO zamowienia (idKlienta, status) VALUES (id_klienta, 'S');
+  SET zidz = LAST_INSERT_ID();
+  SET id_produktow = CONCAT(id_produktu, ',');
+  SET ilosci = CONCAT(ilosc, ',');
+  WHILE LENGTH(id_produktow) > 0 DO
+    SET id_produktu = SUBSTRING_INDEX(id_produktow, ',', 1);
+    SET il = SUBSTRING_INDEX(id_produktow, ',', 1);
+    SET id_produktow = SUBSTRING(id_produktow, (LOCATE(',', id_produktow) + 1));
+    SET ilosci = SUBSTRING(id_produktow, (LOCATE(',', id_produktow) + 1));
+    CALL dodajProduktDoZamowienia(id_produktu, il, id_klienta, zidz);
+  END WHILE;
+END //
